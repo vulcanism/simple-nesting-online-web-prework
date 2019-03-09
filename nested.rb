f6177b8d53b82b5233ffc00d0acdbbbbe845c28d
Savannah C <sgcorrell@gmail.com>
2019-03-09 22:14:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-    programmer_hash[:yukihiro_matsumoto] = {:known_for
+    programmer_hash[:yukihiro_matsumoto] = {known_for
   
 end
 
