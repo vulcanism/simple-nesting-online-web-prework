6678d3abdfe370fbd866131d9991eec20bfb7a55
Savannah C <sgcorrell@gmail.com>
2019-03-09 22:09:37 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -17,7 +17,7 @@ def hopper
      }
 
 return programmer_hash[:grace_hopper]
-return programmer_hash.dig :alan_kay, :known_for
+return programmer_hash[:alan_kay][:known_for]
 end
 
 def alan_kay_is_known_for
